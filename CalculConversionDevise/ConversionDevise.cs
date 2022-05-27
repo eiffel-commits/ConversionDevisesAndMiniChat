3076340ca9aff888e7ed04fb4f80f8aad3cc5ee5
eiffel-commits <84764659+eiffel-commits@users.noreply.github.com>
2022-05-27 12:47:13 +0200

    Version initiale

---
+++ CalculConversionDevise/ConversionDevise.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculConversionDevise
+{
+    public class ConversionDevise
+    {
+        private static Dictionary<string, List<DeviseCibleAvecTaux>> _dicDevise = new Dictionary<string, List<DeviseCibleAvecTaux>>();
+
+        private string _deviseDepart;
+        private double _montantAConvertirInitial;
+        private string _deviseCible;
+
+        private double _montantAConvertirIntermediaire; // nécesaire pour mémoriser les calculs intermédiares
+        private bool _trouve; // nécessaire pour arrêter la recherhce dans l'arbre, le but étant d'être le plus efficace
+
+        public static Dictionary<string, List<DeviseCibleAvecTaux>> DicDevise { get => _dicDevise; set => _dicDevise = value; }
+
+        
+
+        /// <summary>
+        /// Fonction de calcul appelée par l'utilisateur
+        /// </summary>
+        /// <param name="deviseDepart"></param>
+        /// <param name="montantAConvertir"></param>
+        /// <param name="deviseCible"></param>
+        /// <returns>le montant de conversion calculé</returns>
+        public int CalculerConversion(string deviseDepart, double montantAConvertir, string deviseCible)
+        {
+            _deviseDepart = deviseDepart;
+            _montantAConvertirInitial = montantAConvertir;
+            _deviseCible = deviseCible;
+
+            // pour la fonction de calcul récursive
+            _montantAConvertirIntermediaire = _montantAConvertirInitial;
+            _trouve = false;
+
+            Console.WriteLine($"Conversion de {montantAConvertir} {deviseDepart} en {deviseCible} ");
+
+            // on teste que la devise de départ fasse bien partie du dico
+            if (!_dicDevise.ContainsKey(_deviseDepart))
+            {
+                throw new ApplicationException($"la devise de départ {_deviseDepart} n'est pas connue");
+            }
+
+            // on teste que la devise cible fasse bien partie du dico
+            if (!_dicDevise.ContainsKey(_deviseCible))
+            {
+                throw new ApplicationException($"la devise cible {_deviseCible} n'est pas connue");
+            }
+
+            int resulat = CalculerConversion(deviseIntermediaire: deviseDepart);
+
+
+            Console.WriteLine($"Résultat => la conversion de {montantAConvertir} {deviseDepart} est égale à {resulat} {deviseCible} ");
+            Console.WriteLine("-------------");
+
+            return resulat;
+        }
+
+        /// <summary>
+        /// Fonction principale récursive de calcul
+        /// </summary>
+        /// <param name="deviseIntermediaire"></param>
+        /// <returns>le montant intermédiaire de conversion calculé</returns>
+        private int CalculerConversion(string deviseIntermediaire)
+        {
+            // etape 1 => on cherche s'il existe une conversion d'arrivee simplement dans les valeurs du dico pour la clé correspondante à la devise de départ
+            DeviseCibleAvecTaux deviseCibleAvecTauxDirect = _dicDevise[deviseIntermediaire].Where(l => l.DeviseCible.Equals(_deviseCible)).FirstOrDefault();
+            if (deviseCibleAvecTauxDirect != null)
+            {
+                _montantAConvertirIntermediaire = Math.Round(_montantAConvertirIntermediaire * deviseCibleAvecTauxDirect.TauxChange, 4);
+                Console.WriteLine($"Conversion directe trouvée de {deviseIntermediaire} vers {_deviseCible}, montant = {_montantAConvertirIntermediaire}");
+                _trouve = true;
+            }
+            else
+            {
+                // etape 2 => on n'a pas trouvé tout de suite de conversion, on doit balayer chaque valeur du dico pour la devise recherchée et relancer la recherche de façon récursive
+                // en prenant la valeur et en la considérant comme la devise de départ de façon à explorer tout l'arbre
+                // de façon à optimiser, il n'y a pas besoin de balyer tout l'arbe dès qu'une branche a permis de trouver la conversion en direct (étape1),
+                // d'où le test avec le booléen trouve
+                foreach (DeviseCibleAvecTaux deviseCibleIntermediaireAvecTaux in _dicDevise[deviseIntermediaire])
+                {
+                    if (!_trouve)
+                    {
+                        // inutile de chercher une devise cible qui correspond à la devise de départ
+                        if (!deviseCibleIntermediaireAvecTaux.DeviseCible.Equals(_deviseDepart))
+                        {
+                            _montantAConvertirIntermediaire = Math.Round(_montantAConvertirIntermediaire * deviseCibleIntermediaireAvecTaux.TauxChange, 4);
+                            Console.WriteLine($"Conversion intermédiaire trouvée de {deviseIntermediaire} vers {deviseCibleIntermediaireAvecTaux.DeviseCible}, montant = {_montantAConvertirIntermediaire}");
+                            CalculerConversion(deviseCibleIntermediaireAvecTaux.DeviseCible);
+                        }
+                        // c'est un chemin sans issue, on doit poursuivre mais il faut ré-affecter le montant intermédiaire au montant initial
+                        else
+                        {
+                            Console.WriteLine($"Voie sans issue, la devise cible {deviseCibleIntermediaireAvecTaux.DeviseCible} est égale à la devise de départ");
+                            _montantAConvertirIntermediaire = _montantAConvertirInitial;
+                        }
+                    }
+                }
+            }
+
+            // règle de gestion => le résulat doit être arrondi à l'entier supérieur.
+            return (int)Math.Ceiling(_montantAConvertirIntermediaire);
+        }
+    }
+}
---
+++ CalculConversionDevise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CalculConversionDevise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programme de conversion des devises");
+
+            ConversionDevise conversionDevise = new ConversionDevise();
+
+            // mode sans argument => on utilise un jeu avec des devises de test
+            if (args.Length == 0)
+            {
+                // pour les premiers tests, cela a permis de mettre au point l'algo sans dévélopper la lecture fichier dans un premier temps
+                ConversionDevise.DicDevise = InitConfigDevise.InitDicInMemoryForTest();
+
+                try
+                {
+                    //test1
+                    conversionDevise.CalculerConversion(deviseDepart: "EUR", montantAConvertir: 550, deviseCible: "JPY");
+
+                    //test2
+                    conversionDevise.CalculerConversion(deviseDepart: "USD", montantAConvertir: 450, deviseCible: "JPY");
+
+                    //test3
+                    conversionDevise.CalculerConversion(deviseDepart: "USD", montantAConvertir: 150, deviseCible: "CHF");
+
+                    //test4
+                    conversionDevise.CalculerConversion(deviseDepart: "AUD", montantAConvertir: 220, deviseCible: "INR");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            // mode File si un argument est transmis
+            else
+            {
+                string fileName = args[0];
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    // @"D:\Dev\ConversionDevises\CalculConversionDevies\Input.txt"
+                    ConfigDevise configFileDevise = InitConfigDevise.LectureFichier(fileName);
+
+                    ConversionDevise.DicDevise = configFileDevise.DicDevise;
+
+                    //Appel de la conversion avec les valeurs du fichier
+                    conversionDevise.CalculerConversion(configFileDevise.DeviseDepart, configFileDevise.MontantAConvertirInitial, configFileDevise.DeviseCible);
+                }
+            }
+        }
+    }
+}
